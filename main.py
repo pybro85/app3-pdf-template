1e6e6ce5354a20e5c253dee75590d9003ef5e7b4
AAT <AAT@example.com>
2024-04-04 17:03:48 -0800

    Add more pages for each topic

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ for index,row in df.iterrows():
     # Adding a separating line
     pdf.line(10,21,200,21)
     
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
+    
 
 pdf.output('output.pdf')
 
