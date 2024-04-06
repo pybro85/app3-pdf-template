9374389f8ba535bbcf89e679ff4e1fe233501254
AAT <AAT@example.com>
2024-04-05 16:50:27 -0800

    Lines Added

--- main.py
+++ main.py
@@ -2,20 +2,37 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation='P', unit='mm', format = 'A4')
+pdf.set_auto_page_break(auto = False, margin = 0)
 
 df = pd.read_csv("topics.csv")
 
 for index,row in df.iterrows():
     pdf.add_page()
+    
+    #Set the Header for the Master page
     pdf.set_font(family='Times', style='B', size=24)
     # setting text color using RGB color method
     pdf.set_text_color(100,100,100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align = 'L', ln=1)
     # Adding a separating line
-    pdf.line(10,21,200,21)
+    for y in range(20, 285, 10):
+        pdf.line(10,y,200,y)   
+    # Set the Footer for the Master page
+    pdf.ln(265)
+    pdf.set_font(family='Times', style='I', size=8)
+    pdf.set_text_color(180,180,180)
+    pdf.cell(w = 0, h = 10, txt=row["Topic"], align = "R")
     
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+        pdf.ln(275)
+        # Set the Footer for the Subpages
+        pdf.set_font(family='Times', style='I', size=8)
+        pdf.set_text_color(180,180,180)
+        pdf.cell(w = 0, h = 10, txt=row["Topic"], align = "R")
+        for y in range(20, 285, 10):
+            pdf.line(10,y,200,y)
+       
     
 
 pdf.output('output.pdf')
